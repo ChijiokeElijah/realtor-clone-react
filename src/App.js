d4c0db7611ed01234a86252e45e58f9e78fdd207
Chijioke Elijah <elijahchijioke.ce@gmail.com>
2023-05-01 21:48:06 +0100

    add functionality including onChange and on Submit functions to the create listing page

--- src/App.js
+++ src/App.js
@@ -1,45 +1,47 @@
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import Home from './pages/Home'
-import ForgotPassword from './pages/ForgotPassword'
-import Profile from './pages/Profile'
-import SignIn from './pages/SignIn'
-import SignUp from './pages/SignUp'
-import PrivateRoute from "./components/PrivateRoute"
-import Offers from './pages/Offers'
-import Header from './components/Header'
-import { ToastContainer } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css';
-import CreateListing from './pages/CreateListing'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import ForgotPassword from "./pages/ForgotPassword";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import PrivateRoute from "./components/PrivateRoute";
+import Offers from "./pages/Offers";
+import Header from "./components/Header";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import CreateListing from "./pages/CreateListing";
 
 function App() {
   return (
     <>
       <Router>
-        <Header/>
+        <Header />
         <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/forgotPassword' element={<ForgotPassword/>}/>
-          <Route path='/profile' element={<PrivateRoute/>}>
-            <Route path='/profile' element={<Profile/>}/> 
+          <Route path="/" element={<Home />} />
+          <Route path="/forgotPassword" element={<ForgotPassword />} />
+          <Route path="/profile" element={<PrivateRoute />}>
+            <Route path="/profile" element={<Profile />} />
           </Route>
-          <Route path='/sign-up' element={<SignUp/>}/>
-          <Route path='/sign-in' element={<SignIn/>}/>
-          <Route path='/offers' element={<Offers/>}/>
-          <Route path='/create-listing' element={<CreateListing/>}/>
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/create-listing" element={<PrivateRoute />}>
+            <Route path="/create-listing" element={<CreateListing />} />
+          </Route>
         </Routes>
       </Router>
       <ToastContainer
-      position="bottom-center"
-      autoClose={5000}
-      hideProgressBar={false}
-      newestOnTop={false}
-      closeOnClick
-      rtl={false}
-      pauseOnFocusLoss
-      draggable
-      pauseOnHover
-      theme="dark"
-/>  
+        position="bottom-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
     </>
   );
 }
