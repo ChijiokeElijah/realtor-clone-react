151668714d61ae08b8be882c2b1fe6dfd6fcb450
Chijioke Elijah <elijahchijioke.ce@gmail.com>
2023-05-08 22:12:34 +0100

    Add map to the listing page using leaflet and react-leaflet

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -21,6 +21,7 @@ import {
 } from "react-icons/fa";
 import { getAuth } from "firebase/auth";
 import Contact from "../components/Contact";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 // import Contact from "../components/Contact";
 
 export default function Listing() {
@@ -141,7 +142,7 @@ export default function Listing() {
             </li>
             <li className="flex items-center whitespace-nowrap">
               <FaChair className="text-lg mr-1" />
-              {listing.furnished ? "Furnished" : "Not furnished" }
+              {listing.furnished ? "Furnished" : "Not furnished"}
             </li>
           </ul>
           {listing.userRef !== auth.currentUser?.uid && !contactLandlord && (
@@ -159,14 +160,32 @@ export default function Listing() {
             </div>
           )}
           {contactLandlord && (
-            <Contact  userRef={listing.userRef} listing={listing}/>
+            <Contact userRef={listing.userRef} listing={listing} />
           )}
-          
         </div>
         <div
-          className="bg-blue-300 w-full h-[200px] lg-[400px] 
-            z-10 overflow-x-hidden"
-        ></div>
+          className="w-full h-[200px] md:h-[400px]  
+            z-10 overflow-x-hidden mt-6 md:mt-0 md:ml-2"
+        >
+          <MapContainer
+            center={[listing.geolocation.lat, listing.geolocation.lng]}
+            zoom={13}
+            scrollWheelZoom={false}
+            style={{height:"100%", width:"100%"}}
+          >
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker
+              position={[listing.geolocation.lat, listing.geolocation.lng]}
+            >
+              <Popup>
+                {listing.address}
+              </Popup>
+            </Marker>
+          </MapContainer>
+        </div>
       </div>
     </main>
   );
